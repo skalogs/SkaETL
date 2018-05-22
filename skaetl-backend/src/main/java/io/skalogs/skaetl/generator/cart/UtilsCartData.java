a24a0ab68e01e64f8d8dda0d84dd41ad81d46648
scun <nicolas.npoirier@gmail.com>
2018-05-22 16:17:31 +0200

    fix

--- skaetl-backend/src/main/java/io/skalogs/skaetl/generator/cart/UtilsCartData.java
+++ skaetl-backend/src/main/java/io/skalogs/skaetl/generator/cart/UtilsCartData.java
@@ -74,7 +74,7 @@ public class UtilsCartData {
     };
     private final String[] tabCustomerLastName = new String[]{
             "SMITH",
-            ",JOHNSON",
+            "JOHNSON",
             "WILLIAMS",
             "BROWN",
             "JONES",
