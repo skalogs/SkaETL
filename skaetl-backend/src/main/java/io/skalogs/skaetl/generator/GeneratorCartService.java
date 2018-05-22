22d8f20eeee99e9c3386ba25b3c68332cf33f233
jeanlouisboudart <jeanlouis.boudart@gmail.com>
2018-05-22 15:23:18 +0200

    metrics in cart generator

--- skaetl-backend/src/main/java/io/skalogs/skaetl/generator/GeneratorCartService.java
+++ skaetl-backend/src/main/java/io/skalogs/skaetl/generator/GeneratorCartService.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Lists;
 import io.skalogs.skaetl.config.KafkaConfiguration;
 import io.skalogs.skaetl.domain.*;
 import io.skalogs.skaetl.generator.cart.UtilsCartData;
+import io.skalogs.skaetl.service.MetricProcessService;
 import io.skalogs.skaetl.service.ProcessService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -11,19 +12,22 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @Slf4j
 public class GeneratorCartService {
 
     private final ProcessService processService;
+    private final MetricProcessService metricProcessService;
     private final UtilsCartData utilsCartData;
     private final String host;
     private final String port;
     private Random RANDOM = new Random();
 
-    public GeneratorCartService(ProcessService processService, UtilsCartData utilsCartData, KafkaConfiguration kafkaConfiguration) {
+    public GeneratorCartService(ProcessService processService, MetricProcessService metricProcessService, UtilsCartData utilsCartData, KafkaConfiguration kafkaConfiguration) {
         this.processService = processService;
+        this.metricProcessService = metricProcessService;
         this.utilsCartData =  utilsCartData;
         this.host = kafkaConfiguration.getBootstrapServers().split(":")[0];
         this.port = kafkaConfiguration.getBootstrapServers().split(":")[1];
@@ -84,6 +88,7 @@ public class GeneratorCartService {
                     .processOutput(Lists.newArrayList(
                             ProcessOutput.builder().typeOutput(TypeOutput.ELASTICSEARCH).parameterOutput(ParameterOutput.builder().elasticsearchRetentionLevel(RetentionLevel.week).build()).build()))
                     .build());
+
             try {
                 //HACK
                 Thread.sleep(2000);
@@ -91,8 +96,120 @@ public class GeneratorCartService {
             } catch (Exception e) {
                 log.error("Exception createAndActiveProcessConsumer idProcessCardData");
             }
+
+            buildMetrics();
         }
     }
+
+    private void buildMetrics() {
+        List<ProcessMetric> processMetrics = new ArrayList<>();
+        //TX
+        processMetrics.add(ProcessMetric.builder()
+                .idProcess("CART_NB_TX")
+                .name("Cart - Number of Transaction")
+                .sourceProcessConsumers(Lists.newArrayList("idProcessCardData"))
+                .aggFunction("COUNT(*)")
+                .where("type = \"payment\"")
+                .windowType(WindowType.TUMBLING)
+                .size(2)
+                .sizeUnit(TimeUnit.MINUTES)
+                .processOutputs(Lists.newArrayList(toEsOutput()))
+                .build());
+        processMetrics.add(ProcessMetric.builder()
+                .idProcess("CART_NB_TX_FAIL")
+                .name("Cart - Number of Transaction Failed")
+                .sourceProcessConsumers(Lists.newArrayList("idProcessCardData"))
+                .aggFunction("COUNT(*)")
+                .where("type = \"incident\"")
+                .windowType(WindowType.TUMBLING)
+                .size(2)
+                .sizeUnit(TimeUnit.MINUTES)
+                .processOutputs(Lists.newArrayList(toEsOutput()))
+                .build());
+        //PRODUCT
+        processMetrics.add(ProcessMetric.builder()
+                .idProcess("CART_NB_PRODUCT_SEEN")
+                .name("Cart - Number of Product seen")
+                .sourceProcessConsumers(Lists.newArrayList("idProcessCardData"))
+                .aggFunction("COUNT(*)")
+                .where("type = \"showProduct\"")
+                .windowType(WindowType.TUMBLING)
+                .size(2)
+                .sizeUnit(TimeUnit.MINUTES)
+                .processOutputs(Lists.newArrayList(toEsOutput()))
+                .build());
+        processMetrics.add(ProcessMetric.builder()
+                .idProcess("CART_NB_PRODUCT_SEEN")
+                .name("Cart - Number of Product seen per user")
+                .sourceProcessConsumers(Lists.newArrayList("idProcessCardData"))
+                .aggFunction("COUNT(*)")
+                .where("type = \"showProduct\"")
+                .groupBy("customerEmail_ue")
+                .windowType(WindowType.TUMBLING)
+                .size(2)
+                .sizeUnit(TimeUnit.MINUTES)
+                .processOutputs(Lists.newArrayList(toEsOutput()))
+                .build());
+        processMetrics.add(ProcessMetric.builder()
+                .idProcess("CART_NB_PRODUCT_SOLD")
+                .name("Cart - Number of Product Sold")
+                .sourceProcessConsumers(Lists.newArrayList("idProcessCardData"))
+                .aggFunction("COUNT(*)")
+                .where("type = \"addToCart\"")
+                .groupBy("product-name")
+                .windowType(WindowType.TUMBLING)
+                .size(2)
+                .sizeUnit(TimeUnit.MINUTES)
+                .processOutputs(Lists.newArrayList(toEsOutput()))
+                .build());
+        //DISCOUNT
+        processMetrics.add(ProcessMetric.builder()
+                .idProcess("CART_NB_OF_DISCOUNT_PER_USER")
+                .name("Cart - Number of Discount per User")
+                .sourceProcessConsumers(Lists.newArrayList("idProcessCardData"))
+                .aggFunction("COUNT(*)")
+                .where("type = \"payment\"")
+                .groupBy("customerEmail_ue")
+                .windowType(WindowType.TUMBLING)
+                .size(2)
+                .sizeUnit(TimeUnit.MINUTES)
+                .processOutputs(Lists.newArrayList(toEsOutput()))
+                .build());
+        processMetrics.add(ProcessMetric.builder()
+                .idProcess("CART_AVG_DISCOUNT_PER_USER")
+                .name("Cart - Average Discount per User")
+                .sourceProcessConsumers(Lists.newArrayList("idProcessCardData"))
+                .aggFunction("AVG(discount_long)")
+                .where("type = \"payment\"")
+                .groupBy("customerEmail_ue")
+                .windowType(WindowType.TUMBLING)
+                .size(2)
+                .sizeUnit(TimeUnit.MINUTES)
+                .processOutputs(Lists.newArrayList(toEsOutput()))
+                .build());
+        createAndActivateMetrics(processMetrics);
+    }
+
+    private ProcessOutput toEsOutput() {
+        return ProcessOutput.builder()
+                .typeOutput(TypeOutput.ELASTICSEARCH)
+                .parameterOutput(ParameterOutput.builder().elasticsearchRetentionLevel(RetentionLevel.week).build())
+                .build();
+    }
+
+    private void createAndActivateMetrics(List<ProcessMetric> processMetrics) {
+        for (ProcessMetric processMetric : processMetrics) {
+            metricProcessService.updateProcess(processMetric);
+        }
+        for (ProcessMetric processMetric : processMetrics) {
+            try {
+                metricProcessService.activateProcess(processMetric);
+            } catch (Exception e) {
+                log.error("Error occured when activating metrics " + processMetric.getIdProcess(), e);
+            }
+        }
+    }
+
 
     public void generateData(Integer nbCustomer, Integer nbShowByMinute, Integer nbAddToCardByMinute, Integer nbPaySuccessByMinute,Integer nbPayNotSuccessByMinute, Integer timeToGenerateInMinute){
         createAndActiveProcessConsumer();
