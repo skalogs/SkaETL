9b757c017b3235331506406aa9014de582f8462d
jeanlouisboudart <jeanlouis.boudart@gmail.com>
2018-05-28 17:43:55 +0200

    workaround for swagger

--- skaetl-backend/src/main/java/io/skalogs/skaetl/utils/WebConfig.java
+++ skaetl-backend/src/main/java/io/skalogs/skaetl/utils/WebConfig.java
@@ -3,6 +3,7 @@ package io.skalogs.skaetl.utils;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -10,7 +11,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedMethods("GET", "POST", "PUT").allowedOrigins("*");
+
     }
 }
