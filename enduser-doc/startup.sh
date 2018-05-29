3cb89ef3c0abf953c1326bfa41a90046391474a4
jeanlouisboudart <jeanlouis.boudart@gmail.com>
2018-05-29 13:36:56 +0200

    use different delimiter to avoid interpreting slash in url

--- enduser-doc/startup.sh
+++ enduser-doc/startup.sh
@@ -6,11 +6,10 @@ for file in $(find /usr/share/nginx/html/ -name '*.html')
 do
    #echo $file
    sed -i "s/SERVICE_PROJECT_NAME/${SERVICE_PROJECT_NAME}/g" $file
-   sed -i "s/SERVICE_KIBANA_ENDPOINT/${SERVICE_KIBANA_ENDPOINT}/g" $file
-   sed -i "s/SERVICE_ELASTICSEARCH_ENDPOINT/${SERVICE_ELASTICSEARCH_ENDPOINT}/g" $file
-   sed -i "s/SERVICE_KAFKA_ENDPOINT/${SERVICE_KAFKA_ENDPOINT}/g" $file
+   sed -i "s@SERVICE_KIBANA_ENDPOINT@${SERVICE_KIBANA_ENDPOINT}@g" $file
+   sed -i "s@SERVICE_ELASTICSEARCH_ENDPOINT@${SERVICE_ELASTICSEARCH_ENDPOINT}@g" $file
+   sed -i "s@SERVICE_KAFKA_ENDPOINT@${SERVICE_KAFKA_ENDPOINT}@g" $file
    sed -i "s/SERVICE_SUPPORT_EMAIL/${SERVICE_SUPPORT_EMAIL}/g" $file
-   sed -i "s/SERVICE_KAFKA_EVENT_TOPIC/${SERVICE_KAFKA_EVENT_TOPIC}/g" $file
 done
 
 chown -R nginx:nginx /usr/share/nginx/
