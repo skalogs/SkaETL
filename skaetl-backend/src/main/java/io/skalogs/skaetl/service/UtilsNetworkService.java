9eca03c05e560f7a49f1484e9dc0ad436c0b489e
christophefromparis <christophe.cosnefroy@gmail.com>
2018-06-04 12:25:13 +0200

    Appends the links between consumer and metric (and that's the drama)

--- skaetl-backend/src/main/java/io/skalogs/skaetl/service/UtilsNetworkService.java
+++ skaetl-backend/src/main/java/io/skalogs/skaetl/service/UtilsNetworkService.java
@@ -62,10 +62,10 @@ public class UtilsNetworkService {
         String source, target;
 
         //add node Input
-        source = new StringBuilder().append("TOPIC [").append(processConsumer.getProcessInput().getTopicInput()).append("]").toString();
+        source = processConsumer.getIdProcess();
         mapNode.put(source, NetworkNodeWeb.builder()
                 .id(source)
-                .name(source)
+                .name(new StringBuilder().append("Topic [").append(processConsumer.getProcessInput().getTopicInput()).append("]").toString())
                 .color(getColor(TypeOutput.KAFKA))
                 .build());
 
@@ -73,7 +73,7 @@ public class UtilsNetworkService {
         for (ProcessOutput processOutput : processConsumer.getProcessOutput()) {
 
             if (processOutput.getTypeOutput() == TypeOutput.KAFKA)
-                target = new StringBuilder().append("TOPIC [").append(processOutput.getParameterOutput().getTopicOut()).append("]").toString();
+                target = new StringBuilder().append("Topic [").append(processOutput.getParameterOutput().getTopicOut()).append("]").toString();
             else
                 target = processOutput.getTypeOutput().name();
 
@@ -89,28 +89,56 @@ public class UtilsNetworkService {
                     .sid(source)
                     .tid(target)
                     .color("green")
-                    .name(processConsumer.getName())
+                    .name(new StringBuilder().append("Consumer process [").append(processConsumer.getName()).append("]").toString())
                     .build());
         }
     }
 
     private void addMetricNodeLink(ProcessMetric processMetric, Map<String, NetworkNodeWeb> mapNode, List<NetworkLinksWeb> listLink) {
 
-        String source, target;
+        String source, process, target;
+
+        // Adds processes
+        process = new StringBuilder().append("Metric process [").append(processMetric.getName()).append("]").toString();
+        mapNode.put(process, NetworkNodeWeb.builder()
+                .id(process)
+                .name(process)
+                .color("DarkBlue")
+                .build());
 
         // Adds input nodes
-        source = new StringBuilder().append("TOPIC [").append(processMetric.getFromTopic()).append("]").toString();
-        mapNode.put(source, NetworkNodeWeb.builder()
-                .id(source)
-                .name(source)
-                .color(getColor(TypeOutput.KAFKA))
-                .build());
+        for (String processInput : processMetric.getSourceProcessConsumers()) {
+            source = new StringBuilder().append("Stream [").append(processInput).append("]").toString();
+            mapNode.put(source, NetworkNodeWeb.builder()
+                    .id(source)
+                    .name(source)
+                    .color("Crimson")
+                    .build());
+
+            // Adds input links
+            listLink.add(NetworkLinksWeb.builder()
+                    .id(source + process)
+                    .sid(source)
+                    .tid(process)
+                    .name(" ")
+                    .color("red")
+                    .build());
+
+            // Adds links between consumer and metric
+            listLink.add(NetworkLinksWeb.builder()
+                    .id(processInput + source)
+                    .sid(processInput)
+                    .tid(source)
+                    .name("stream")
+                    .color("orange")
+                    .build());
+        }
 
         // Adds output nodes
         for (ProcessOutput processOutput : processMetric.getProcessOutputs()) {
 
             if (processOutput.getTypeOutput() == TypeOutput.KAFKA)
-                target = new StringBuilder().append("TOPIC [").append(processOutput.getParameterOutput().getTopicOut()).append("]").toString();
+                target = new StringBuilder().append("Topic [").append(processOutput.getParameterOutput().getTopicOut()).append("]").toString();
             else
                 target = processOutput.getTypeOutput().name();
 
@@ -120,13 +148,13 @@ public class UtilsNetworkService {
                     .color(getColor(processOutput.getTypeOutput()))
                     .build());
 
-            // Adds links
+            // Adds output links
             listLink.add(NetworkLinksWeb.builder()
-                    .id(source + target)
-                    .sid(source)
+                    .id(process + target)
+                    .sid(process)
                     .tid(target)
+                    .name(" ")
                     .color("red")
-                    .name(processMetric.getName())
                     .build());
         }
     }
