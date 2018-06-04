77d9fe3bb304728164daefcc4d434e8abfd0398d
jeanlouisboudart <jeanlouis.boudart@gmail.com>
2018-06-04 18:00:05 +0200

    cosmetics

--- skaetl-frontend/src/components/process/FiltersHelp.vue
+++ skaetl-frontend/src/components/process/FiltersHelp.vue
@@ -1,25 +1,30 @@
 <template>
-  <v-card>
-    <v-card-title>
-      <b>Language Syntax</b>
-      <v-spacer></v-spacer>
-      <v-text-field
-        v-model="search"
-        append-icon="search"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field></v-card-title>
-    <v-card-text>
-      <v-data-table :headers="headers" :items="filterFunctions" :search="search">
-        <template slot="items" slot-scope="props">
-          <td>{{props.item.name}}</td>
-          <td>{{props.item.description}}</td>
-          <td>{{props.item.example}}</td>
-        </template>
-      </v-data-table>
-    </v-card-text>
-  </v-card>
+  <v-expansion-panel expand>
+    <v-expansion-panel-content >
+      <div slot="header">Language Syntax</div>
+      <v-card color="grey lighten-3">
+        <v-card-title>
+          <b>List of functions</b>
+          <v-spacer></v-spacer>
+          <v-text-field
+            v-model="search"
+            append-icon="search"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field></v-card-title>
+        <v-card-text>
+          <v-data-table :headers="headers" :items="filterFunctions" :search="search">
+            <template slot="items" slot-scope="props">
+              <td>{{props.item.name}}</td>
+              <td>{{props.item.description}}</td>
+              <td>{{props.item.example}}</td>
+            </template>
+          </v-data-table>
+        </v-card-text>
+      </v-card>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
 </template>
 
 
