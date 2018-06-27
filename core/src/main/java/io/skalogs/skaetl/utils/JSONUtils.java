ca5c69f00eaddb4a112b6ef9ee3cc34f5625b068
jeanlouisboudart <jeanlouis.boudart@gmail.com>
2018-06-27 07:52:36 +0200

    ignore non null properties globally

--- core/src/main/java/io/skalogs/skaetl/utils/JSONUtils.java
+++ core/src/main/java/io/skalogs/skaetl/utils/JSONUtils.java
@@ -1,5 +1,6 @@
 package io.skalogs.skaetl.utils;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -17,6 +18,7 @@ public class JSONUtils {
     private JSONUtils() {
         objectMapper = new ObjectMapper();
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         objectMapper.setDateFormat(new ISO8601DateFormat());
     }
 
