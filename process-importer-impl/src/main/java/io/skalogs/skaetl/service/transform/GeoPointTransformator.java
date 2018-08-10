a5d43805779879950f49baf616af2a02f42fede9
jeanlouisboudart <jeanlouis.boudart@gmail.com>
2018-08-10 15:30:23 +0200

    support for non text geopoint

--- process-importer-impl/src/main/java/io/skalogs/skaetl/service/transform/GeoPointTransformator.java
+++ process-importer-impl/src/main/java/io/skalogs/skaetl/service/transform/GeoPointTransformator.java
@@ -1,11 +1,11 @@
 package io.skalogs.skaetl.service.transform;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.skalogs.skaetl.domain.ParameterTransformation;
 import io.skalogs.skaetl.domain.TypeValidation;
 import io.skalogs.skaetl.service.TransformatorProcess;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 
 @Slf4j
 public class GeoPointTransformator extends TransformatorProcess {
@@ -15,9 +15,9 @@ public class GeoPointTransformator extends TransformatorProcess {
     }
 
     public void apply(String idProcess, ParameterTransformation parameterTransformation, ObjectNode jsonValue) {
-        String valueToFormat = at(parameterTransformation.getKeyField(), jsonValue).asText();
-        if (StringUtils.isNotBlank(valueToFormat)) {
-            put(jsonValue, parameterTransformation.getKeyField() + "_gp", valueToFormat);
+        if (has(parameterTransformation.getKeyField(),jsonValue)) {
+            JsonNode jsonNode = at(parameterTransformation.getKeyField(), jsonValue);
+            put(jsonValue, parameterTransformation.getKeyField() + "_gp", jsonNode);
             remove(jsonValue,parameterTransformation.getKeyField());
         }
     }
